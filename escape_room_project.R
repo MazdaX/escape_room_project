59a06851edc842a21e5324e07fce4943ba7fd617
mazdax <mazdak.salavati@gmail.com>
2019-03-22 23:51:23 +0000

    Done

---
+++ escape_room_project.R
@@ -0,0 +1,58 @@
+#Collecting metadata on edinburgh
+#1. Locked in Edinburgh 1 Summerhall | Based within the Summerhall Building., Edinburgh EH9 1PL, Scotland +  875 reviews 5 star with 2 bar in walking distance + 16 restaurant walking distance
+#1.1 The cutting room 5/5 difficulty min 3 max 8
+#1.2 The distillary 3/5 difficulty min 2 max 5 
+#1.3 The secret lab 4/5 difficulty min4 max 10
+# distance from work 6.7miles 
+
+#2. Escape Hunt Edinburgh 24 Castle Street | Upper Floors, Edinburgh EH2 3HT, Scotland + 158 reviews + 10 bars + 10 resaurants
+# 2.1 Escape the wild west min 2 max 6 60min 4/5 
+# 2.2 Blackbeards treasure min2 max 6 60min 4/5 
+# 2.3 The forth samurai min 2 max 6 60min 3/5 
+# 2.4 The last vikings min 2 max 6 60min 4/5 
+# Distance from work 6.9
+
+
+# 3. Padlox escape room 37 Bernard Street, Edinburgh EH6 6SH, Scotland + 185 reviews + 15 restaurants + 14 bars and pubs
+# 3.1 Going for gold 1 ours min2 max 6 4/5 
+#distance from work 13.9
+
+
+# 4. Escape reality Edinburgh 1 Fountainbridge Square, Edinburgh EH3 9QB, Scotland + 400 reviews + 3 restaurants + 4 bars  
+#   4.1 JUNGALA min 2 max 6 difficulty 3/5 
+#   4.2 TORTUGA PIRATES min 2 max 6 difficulty 3/5 
+#   4.3 MACHINA min 2 max 6 difficulty 4/5
+#   4.4 THE ASYLUM min2 max 6 difficulty 4/5 
+#   4.5 ALCATRAZ min 2 max 6 difficult 5/5
+#   4.6 NOSFERATU min 2 max 6 difficulty 4/5 
+# distance from work 6.2 miels 
+
+# 5. Can you scape? 5 Holyrood Road, Edinburgh EH8 8AE, Scotland + 836 reviews + 7  restarurans  + 13 bars and pubs
+#   5.1 Operation spellbound min 2 max 10 5/5 
+#   5.2 Operation Odyssey min 2 max 4 4/5
+# distance from work 7.2 miles 
+  
+# 6. Exit plan Edinburgh 3 Queen Street, Edinburgh EH2 1JE, Scotland + 491 reviews +  9 res + 5 bars and pubs 
+# 6.1 The tesla cube min 2 max 7 3/5 
+# 6.2 Svengalis lair min 2 max 6 4/5 
+# 6.3 Framed! min 2 max 6 3/5 
+# distance from work 7.3 miles 
+
+# 7. Department of magic 9-11 Blair Street, Edinburgh EH1 1QR, Scotland + 160 revs + 4 res + 14 bars and pubs 
+#   7.1 Prophecies quest min 2 max 5 3/5 
+#   7.2 Dark lord resurrection min 2 max 5 3/5 
+#   7.3 Magic potion Tavern min 2 max 5 4/5 
+# distance from work 7.1 miles 
+
+# # 8. Escape Edinburgh New town, 1 St. Colme Street | Citibase Basement, Edinburgh EH3 6AA, Scotland + 836 rev + 11 res + 12 bars and pubs 
+# 8.1 The magic emporium min 2 max 6 3/5
+# 8.2 221B Baker street min 2  max 5 4/5 
+# 8.3 The experiment min 2 max 6 3/5 
+# 8.4 Area51 min 2 max 5 4/5
+# 8.5 Houdinis workshop min 2 max 6 3/5
+# 8.6 Casino min 2 max 6 3/5
+# 8.7 Contagion min 2 max 6 3/5 
+# distance from work 7.1 
+
+
+  
